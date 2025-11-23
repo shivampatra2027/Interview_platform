cb7ec48b462b9b00082e9eb5389825a960738d80
Your Name <you@example.com>
2025-11-23 17:47:48 +0530

    minor fix

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,1 +1,15 @@
 //jwt.js
+// src/utils/jwt.js
+
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
+
+export const generateToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    JWT_SECRET,
+    {
+      expiresIn: "7d" 
+    }
+  );
+};
--- backend/src/utils/response.js
+++ backend/src/utils/response.js
@@ -1,1 +1,27 @@
 //response.js
+
+// src/utils/response.js
+
+export const success = (
+  res,
+  data = {},
+  message = "Success",
+  statusCode = 200
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+export const failure = (
+  res,
+  message = "Something went wrong",
+  statusCode = 500
+) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
